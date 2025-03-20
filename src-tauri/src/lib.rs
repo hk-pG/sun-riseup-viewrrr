a59730b0a78f3b215675e6cf1f8371d2112e5732
hk-pG <hukada.gogotea@gmail.com>
2025-03-20 14:50:53 +0900

    feat: ファイルアクセスAPIを実装
    指定パス直下のファイルを一覧するコマンドメソッドを実装
    指定パス直下の画像ファイル名を一覧するコマンドメソッドを実装

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,7 @@
+use std::fs;
+use std::path::PathBuf;
+use walkdir::WalkDir;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,7 +12,36 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![list_folders, list_images])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+// フォルダ一覧取得API
+#[tauri::command]
+fn list_folders(current_dir: String) -> Vec<String> {
+    let path = PathBuf::from(current_dir);
+    fs::read_dir(path)
+        .unwrap()
+        .filter_map(|entry| entry.ok())
+        .filter(|entry| entry.path().is_dir())
+        .map(|entry| entry.file_name().into_string().unwrap())
+        .collect()
+}
+
+// 画像一覧取得API
+#[tauri::command]
+fn list_images(folder: String) -> Vec<String> {
+    let path = PathBuf::from(folder);
+    WalkDir::new(path)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|entry| {
+            entry
+                .path()
+                .extension()
+                .is_some_and(|ext| ["png", "jpg", "jpeg", "webp"].contains(&ext.to_str().unwrap()))
+        })
+        .map(|entry| entry.path().to_string_lossy().to_string())
+        .collect()
 }
