6d8b0bcbc5ef7a14326697f98495eed15ebee87c
hukada.gogotea <hukada.gogotea@gmail.com>
2025-05-08 22:59:58 +0900

    画像の表示に成功

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
+import { open } from "@tauri-apps/plugin-dialog";
+import { convertFileSrc } from "@tauri-apps/api/core";
+
+import "./App.css";
+
+function App() {
+	const [images, setImages] = useState<string[]>([]);
+	const [current, setCurrent] = useState(0);
+
+	async function selectFolder() {
+		const folder = await open({ directory: true });
+		if (folder && typeof folder === "string") {
+			const files = await invoke("list_images_in_folder", {
+				folderPath: folder,
+			});
+
+			if (Array.isArray(files)) {
+				// TODO: should be used type guard instead of type assertion
+				// convert using convertFileSrc
+				const assetPaths = files.map((file) => convertFileSrc(file));
+				setImages(assetPaths as string[]);
+				setCurrent(0);
+			}
+			setCurrent(0);
+		}
+	}
+
+	return (
+		<div>
+			<button type="button" onClick={selectFolder}>
+				Select Image Folder
+			</button>
+			{images.length > 0 && (
+				<img
+					alt="selected file in folder"
+					src={`${images[current]}`}
+					style={{
+						maxWidth: "100%",
+						maxHeight: "100%",
+					}}
+				/>
+			)}
+		</div>
+	);
+}
+
+export default App;
