f28313b42612f990b9afe3686a1ec5261efcce02
hukada.gogotea <hukada.gogotea@gmail.com>
2025-05-10 23:25:39 +0900

    useEffectを利用し、キーボードと画面上のボタンから表示する画像を左右に切り替える機能を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import { open } from "@tauri-apps/plugin-dialog";
 import { convertFileSrc } from "@tauri-apps/api/core";
@@ -6,19 +6,26 @@ import { convertFileSrc } from "@tauri-apps/api/core";
 import "./App.css";
 
 function App() {
+	// ユーザーの選択した1つのフォルダ内の画像のパスを格納する
 	const [images, setImages] = useState<string[]>([]);
+	// 現在表示している画像のインデックス
 	const [current, setCurrent] = useState(0);
 
+	/**
+	 * ダイアログを開いてユーザーにフォルダを選択させる
+	 */
 	async function selectFolder() {
+		// ダイアログを開いてフォルダを選択させる
 		const folder = await open({ directory: true });
 		if (folder && typeof folder === "string") {
+			// フォルダ内の画像を取得する
 			const files = await invoke("list_images_in_folder", {
 				folderPath: folder,
 			});
 
 			if (Array.isArray(files)) {
 				// TODO: should be used type guard instead of type assertion
-				// convert using convertFileSrc
+				// 画像のパスをWebViewで表示できる形式に変換する
 				const assetPaths = files.map((file) => convertFileSrc(file));
 				setImages(assetPaths as string[]);
 				setCurrent(0);
@@ -27,12 +34,28 @@ function App() {
 		}
 	}
 
+	useEffect(() => {
+		const handler = (e: KeyboardEvent) => {
+			if (e.key === "ArrowRight") {
+				setCurrent((c) => Math.min(c + 1, images.length - 1));
+			} else if (e.key === "ArrowLeft") {
+				setCurrent((c) => Math.max(c - 1, 0));
+			}
+		};
+
+		// キーボードの左右キーで画像を切り替える
+		window.addEventListener("keydown", handler);
+
+		// コンポーネントがアンマウントされたときにイベントリスナーを削除する
+		return () => window.removeEventListener("keydown", handler);
+	}, [images]);
+
 	return (
-		<div>
+		<>
 			<button type="button" onClick={selectFolder}>
 				Select Image Folder
 			</button>
-			{/* current image */}
+			{/* 現在表示している画像 */}
 			{images.length > 0 && (
 				<img
 					alt="selected file in folder"
@@ -43,40 +66,45 @@ function App() {
 					}}
 				/>
 			)}
-			{/* images in folder selected */}
-			<div style={{ display: "flex", overflowX: "scroll" }}>
-				{images.map((path, index) => (
-					<>
-						<button
-							key={path}
-							type="button"
-							onClick={() => {
-								setCurrent(index);
-							}}
-							style={{
-								backgroundColor: index === current ? "blue" : "white",
-								color: index === current ? "white" : "black",
-								maxHeight: "50px",
-								maxWidth: "50px",
-							}}
-						>
-							<img
-								key={path}
-								alt="selected file in folder"
-								src={`${path}`}
-								style={{
-									margin: "5px",
-									cursor: "pointer",
-									maxWidth: "100%",
-									maxHeight: "100%",
-								}}
-							/>
-							{index}
-						</button>
-					</>
-				))}
+
+			{/* 画像のインデックスを表示 */}
+			<div>
+				<p>
+					{current + 1} / {images.length}
+				</p>
 			</div>
-		</div>
+			<div
+				style={{
+					display: "flex",
+					justifyContent: "center",
+					width: "100%",
+					position: "absolute",
+					bottom: 0,
+				}}
+			>
+				<button
+					type="button"
+					onClick={() => {
+						// 左矢印が押されたとき
+						setCurrent((c) => Math.max(c - 1, 0));
+					}}
+				>
+					{/* 左矢印 */}
+					<span>&lt;</span>
+				</button>
+
+				<button
+					type="button"
+					onClick={() => {
+						// 右矢印が押されたとき
+						setCurrent((c) => Math.min(c + 1, images.length - 1));
+					}}
+				>
+					{/* 右矢印 */}
+					<span>&gt;</span>
+				</button>
+			</div>
+		</>
 	);
 }
 
