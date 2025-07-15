83986faeeec6bfee923cc2eb2ac54afb67b9eae9
hukada.gogotea <hukada.gogotea@gmail.com>
2025-07-15 22:19:43 +0900

    fix: インターフェース定義にコメントを追加

--- src/service/FileSystemService.ts
+++ src/service/FileSystemService.ts
@@ -6,7 +6,26 @@ export interface FileSystemService {
   getDirName(filePath: string): Promise<string>;
 
   // ユーザー定義コマンド
+
+  /**
+   * フォルダ内の画像ファイルをリストアップする
+   * @param {string} folderPath - 画像ファイルをリストアップするフォルダのパス
+   * @returns {Promise<string[]>} - 画像ファイルのパスの配列
+   * @throws {Error} - 画像ファイルのリストアップ中にエラーが発生した場合
+   */
   listImagesInFolder(folderPath: string): Promise<string[]>;
+
+  /**
+   * 指定されたフォルダと同じ階層にあるフォルダのリストを取得する
+   * @param currentFolderPath 現在のフォルダのパス
+   * @return {Promise<string[]>} 同じ階層にあるフォルダのパスの配列
+   */
   getSiblingFolders(currentFolderPath: string): Promise<string[]>;
+
+  /**
+   * ファイルのパスをリソースURLに変換する
+   * @param filePath ファイルのパス
+   * @return {string} リソースURL
+   */
   convertFileSrc(filePath: string): string;
 }
