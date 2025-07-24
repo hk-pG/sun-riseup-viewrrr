e95ac10ea70a1402b1c1ad4c0999c54603f18bf6
Rabi-roid <nii23201.fukada@gmail.com>
2025-07-24 04:06:07 +0000

    feat: 簡易的にstorybookにもFolderListを作成

---
+++ src/stories/FolderList.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { FolderList } from '../components/FolderList';
+import type { FolderInfo } from '../types/viewerTypes';
+import { mockImageSourcesByFolderPath } from '../../data/mockData';
+
+const folders: FolderInfo[] = [
+  {
+    path: '/test_images/folder_1',
+    name: 'フォルダ1',
+    imageCount: mockImageSourcesByFolderPath['/test_images/folder_1']?.length,
+  },
+  {
+    path: '/test_images/folder_2',
+    name: 'フォルダ2',
+    imageCount: mockImageSourcesByFolderPath['/test_images/folder_2']?.length,
+  },
+  {
+    path: '/mock/empty-folder',
+    name: '空のフォルダ',
+    imageCount: 0,
+  },
+];
+
+const meta: Meta<typeof FolderList> = {
+  title: 'Viewer/FolderList',
+  component: FolderList,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+};
+export default meta;
+
+type Story = StoryObj<typeof FolderList>;
+
+export const Default: Story = {
+  args: {
+    folders,
+    selectedFolder: folders[0],
+    onFolderSelect: (folder: FolderInfo) => alert(`選択: ${folder.name}`),
+    onFolderDoubleClick: (folder: FolderInfo) =>
+      alert(`ダブルクリック: ${folder.name}`),
+    thumbnailSize: 80,
+    showImageCount: true,
+  },
+};
+
+export const NoSelection: Story = {
+  args: {
+    folders,
+    selectedFolder: undefined,
+    onFolderSelect: () => {},
+    onFolderDoubleClick: () => {},
+    thumbnailSize: 80,
+    showImageCount: true,
+  },
+};
+
+export const Empty: Story = {
+  args: {
+    folders: [],
+    selectedFolder: undefined,
+    onFolderSelect: () => {},
+    onFolderDoubleClick: () => {},
+    thumbnailSize: 80,
+    showImageCount: true,
+  },
+};
