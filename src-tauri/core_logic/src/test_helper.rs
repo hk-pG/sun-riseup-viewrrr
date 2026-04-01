e88a07c2a539dbd4bba93f8404a5e01727342585
hk-pG <hukada.gogotea@gmail.com>
2026-04-01 22:22:56 +0900

    feat: ZIPファイルを作成する関数を追加

--- src-tauri/core_logic/src/test_helper.rs
+++ src-tauri/core_logic/src/test_helper.rs
@@ -2,9 +2,12 @@
 pub mod test_helpers {
     use std::{
         env::temp_dir,
-        fs::{create_dir_all, remove_dir_all},
-        path::PathBuf,
+        fs::{create_dir_all, remove_dir_all, File},
+        io::{Read, Write},
+        path::{Path, PathBuf},
     };
+
+    use zip::write::SimpleFileOptions;
 
     pub struct TempTestDir {
         pub path: PathBuf,
@@ -31,5 +34,33 @@ pub mod test_helpers {
         fn drop(&mut self) {
             let _ = remove_dir_all(&self.path);
         }
+    }
+
+    pub fn create_zip<P: AsRef<Path>>(zip_path: P, files: Vec<P>) -> zip::result::ZipResult<()> {
+        let zip_file = File::create(zip_path)?;
+        let mut zip = zip::ZipWriter::new(zip_file);
+
+        let options = SimpleFileOptions::default()
+            .compression_method(zip::CompressionMethod::Deflated)
+            .large_file(false);
+
+        for file in files {
+            let file_path = file.as_ref();
+            let name = file_path
+                .file_name()
+                .unwrap()
+                .to_string_lossy()
+                .into_owned();
+
+            zip.start_file(&name, options)?;
+
+            let mut f = File::open(file_path)?;
+            let mut buffer = Vec::new();
+            f.read_to_end(&mut buffer)?;
+            zip.write_all(&buffer)?;
+        }
+
+        zip.finish()?;
+        Ok(())
     }
 }
